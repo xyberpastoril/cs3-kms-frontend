aa0e40238f04a9d0e0b821bebcbdaf0394730fa4
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-08-01 17:23:54 +0800

    Update routes
    Add `about`, `terms`, `privacy`
    Update `questions` to `search`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { ForgotComponent } from './auth/forgot/forgot.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ResetComponent } from './auth/reset/reset.component';
 import { CategoryComponent } from './core/category/category.component';
 import { HomeComponent } from './core/home/home.component';
-import { QuestionComponent } from './core/question/question.component';
+import { SearchComponent } from './core/question/search/search.component';
 import { ShowComponent } from './core/question/show/show.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
   {
@@ -26,8 +29,8 @@ const routes: Routes = [
     component: ForgotComponent,
   },
   {
-    path: 'questions',
-    component: QuestionComponent,
+    path: 'search',
+    component: SearchComponent,
   },
   {
     path: 'question',
@@ -41,6 +44,18 @@ const routes: Routes = [
   {
     path: 'categories',
     component: CategoryComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'terms',
+    component: TermsComponent
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent
   }
 ];
 
